4142cb53bd106854d49d86da7db8ba94dbd1ec84
itzDingo <polerie2@gmail.com>
2026-06-04 11:52:11 +0100

    Update format selector to bestaudio/best

--- src/utils/MusicQueue.js
+++ src/utils/MusicQueue.js
@@ -218,7 +218,7 @@ class MusicQueue {
 
     const args = [
       '-f',
-      'bestaudio',
+      'bestaudio/best',
       '--no-playlist',
       '--no-warnings',
       '--no-check-certificates',
